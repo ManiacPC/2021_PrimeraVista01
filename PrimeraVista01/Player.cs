1306c0f290915af6ce0ee03ef2ef40c8d0b821bc
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-03-29 14:29:56 -0300

    Agregar archivos de proyecto.

---
+++ PrimeraVista01/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraVista01
+{
+    class Player
+    {
+        // atributos
+        private string nickname;
+        private int hp = 100; // health points = puntos de vida
+        private int mp = 50; // mana points = 50 por default
+        // EVENTO AÑO CHINO: SE REGALA DOBLE MP
+
+        // propiedades
+        public string Nickname { get => nickname; set => nickname = value; }
+        public int Hp { get => hp; set => hp = value; }
+        public int Mp { 
+            get => mp; 
+            set {
+                mp = value * 2; 
+            }
+        }
+
+        // constructor
+        public Player(string nombre)
+        {
+            this.nickname = nombre;
+            Console.WriteLine(nombre);
+            Mp = 50;
+            Console.WriteLine("Magia:" + mp);
+        }
+
+        // métodos
+        public bool atacar()
+        {
+            Random random = new Random();
+            int failChance = random.Next(0,1);
+
+            /*if(failChance == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }*/
+
+            bool resultado = (failChance == 0 ? true : false); // operador ternario
+
+            return resultado; 
+        }        
+    }
+}
---
+++ PrimeraVista01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; // imports?
+
+namespace PrimeraVista01 // package?
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // configuración de consola
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            /* POO: Pruebas y demostraciones */
+            Player jugadorUno = new Player("AnakinWolf31");            
+
+            // salida
+            Console.WriteLine("Esta es una app de consola");
+            //Console.WriteLine("Se ha conectado el jugador " + jugadorUno.Nickname);
+            Console.WriteLine($"Se ha conectado el jugador {jugadorUno.Nickname}");
+
+            // pausa antes del cierre de consola
+            Console.ReadKey();
+        }
+    }
+}
